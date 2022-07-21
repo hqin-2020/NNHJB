2d4fbffe3ff4c38b3d674f69124a20c4a27a47e8
qhaomin <qhaomin@mfe01.chicagobooth.edu>
2022-07-21 14:43:58 -0500

    1

---
+++ src/bash/psie_0500_psih_0500_chi_1000_gammah_5000_gammae_1000.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#SBATCH --account=pi-lhansen
+#SBATCH --job-name=psie_0500_psih_0500_chi_1000_gammah_5000_gammae_1000
+#SBATCH --output=./job-outs/psie_0500_psih_0500_chi_1000_gammah_5000_gammae_1000.out
+#SBATCH --error=./job-outs/psie_0500_psih_0500_chi_1000_gammah_5000_gammae_1000.err
+#SBATCH --time=0-1:00:00
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=64G
+
+module load python/booth/3.8/3.8.5
+module load cuda/11.4
+
+source ~/venv/tensorflow-gpu/bin/activate
+
+srun python3 standard_BFGS.py --model psie_0500_psih_0500_chi_1000_gammah_5000_gammae_1000
+srun python3 standard_Plots.py --model psie_0500_psih_0500_chi_1000_gammah_5000_gammae_1000
+
