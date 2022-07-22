ce8fc4a156dd204d7c86e13facc16b289cb2c3e1
qhaomin <qhaomin@mfe02.chicagobooth.edu>
2022-07-21 23:48:55 -0500

    1

---
+++ src/bash/run_gammah_8000_gammae_1000.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#SBATCH --account=pi-lhansen
+#SBATCH --job-name=run_gammah_8000_gammae_1000
+#SBATCH --output=./job-outs/run_gammah_8000_gammae_1000.out
+#SBATCH --error=./job-outs/run_gammah_8000_gammae_1000.err
+#SBATCH --time=0-2:00:00
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=64G
+
+module load python/booth/3.8/3.8.5
+module load cuda/11.4
+
+source ~/venv/tensorflow-gpu/bin/activate
+
+srun python3 standard_BFGS.py --model gammah_8000_gammae_1000
+srun python3 standard_Plots.py --model gammah_8000_gammae_1000
+
